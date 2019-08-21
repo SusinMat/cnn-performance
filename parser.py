8d521801b2bfbf354102edeb9e48132e560255d4
SusinMat <57694766+SusinMat@users.noreply.github.com>
2019-08-21 07:49:13 -0300

    Add function to parse mean and stddev

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import sys
 import enum
 
@@ -26,45 +27,52 @@ class Layer:
         self.extra_energy = 0.0
         self.extra_power = 0.0
 
-original = []
-approx = []
+average_number_pattern = re.compile(r".+\s+(?P<mean>\d+\.\d+)\s+\w+\s+\(.(?P<stddev>\d+\.\d+)\)")
+def mean_and_stddev(lines, i):
+        match = average_number_pattern.search(lines[i])
+        mean = float(match.group("mean"))
+        stddev = float(match.group("stddev"))
+        return (mean, stddev)
 
-if len(sys.argv) != 2:
-    print("Usage: ./parser.py [profiling_file.out]")
-    exit(1)
+if __name__ == "__main__":
+    original = []
+    approx = []
 
-original_file = sys.argv[1]
-approx_file = "reconstructed_" + original_file
+    if len(sys.argv) != 2:
+        print("Usage: ./parser.py [profiling_file.out]")
+        exit(1)
 
-original_lines = [line for line in [line.rstrip("\n") for line in open(original_file).readlines()] if line != ""]
-if os.path.isfile(approx_file):
-    approx_lines = [line for line in [line.rstrip("\n") for line in open(approx_file).readlines()] if line != ""]
-else:
-    approx_lines = None
+    original_file = sys.argv[1]
+    approx_file = "reconstructed_" + original_file
 
-
-original_i = 0
-conv_count = 0
-state = State.NEW_OP
-
-while "- PROFILER -" not in original_lines[original_i]:
-    original_i += 1
-original_i += 1
-
-while "----------------" not in original_lines[original_i]:
-    if state == State.NEW_OP:
-        new_op = Layer()
-        new_op.op_name = original_lines[original_i]
-        if new_op.op_name in ["CONV_2D", "Dragunov_Slicing"]:
-            conv_count += 1
-        if "Dragunov_" in new_op.op_name:
-            state = State.DRAGUNOV
-        else:
-            state = State.BUILTIN_OP
-        original.append(new_op)
-        satate = State.NEW_OP
+    original_lines = [line for line in [line.rstrip("\n") for line in open(original_file).readlines()] if line != ""]
+    if os.path.isfile(approx_file):
+        approx_lines = [line for line in [line.rstrip("\n") for line in open(approx_file).readlines()] if line != ""]
+    else:
+        approx_lines = None
 
 
+    original_i = 0
+    conv_count = 0
+    state = State.NEW_OP
+
+    while "- PROFILER -" not in original_lines[original_i]:
+        original_i += 1
     original_i += 1
 
 
+    while "----------------" not in original_lines[original_i]:
+        if state == State.NEW_OP:
+            new_op = Layer()
+            new_op.op_name = original_lines[original_i]
+            if new_op.op_name in ["CONV_2D", "Dragunov_Slicing"]:
+                conv_count += 1
+            if "Dragunov_" in new_op.op_name:
+                state = State.DRAGUNOV
+                original_i += 6 * 4
+            else:
+                state = State.BUILTIN_OP
+                (mean, stddev) = mean_and_stddev(original_lines, original_i + 1)
+                original_i += 4
+            original.append(new_op)
+            state = State.NEW_OP
