e85bf22c35e766a7f11aa3366401844b1121b6f0
SusinMat <57694766+SusinMat@users.noreply.github.com>
2019-08-27 18:21:50 -0300

    Add profiling for squeezenet, resnet_v2, inception_resnet_v2

--- run-kbench.sh
+++ run-kbench.sh
@@ -1,8 +1,70 @@
 #!/usr/bin/env bash
 
+
+# echo "original_squeezenet.tflite"
+# ~/kbench/scripts/kbench.py tflite \
+# ~/benchmark-tfl/build/rpi/ \
+# --model-and-label ~/copy-conv-approximation/dump_parser/original_squeezenet.tflite ~/conv-approximation/dump_parser/labels.txt \
+# --inferences 8 \
+# --device-type raspbian \
+# --device-host beholder \
+# --energy \
+# --ignore-tunnel
+#
+# echo "################################################################"
+#
+# echo "approximate_squeezenet.tflite"
+# ~/kbench/scripts/kbench.py tflite \
+# ~/benchmark-tfl/build/rpi/ \
+# --model-and-label ~/copy-conv-approximation/dump_parser/approximate_squeezenet.tflite ~/conv-approximation/dump_parser/labels.txt \
+# --inferences 8 \
+# --device-type raspbian \
+# --device-host beholder \
+# --energy \
+# --ignore-tunnel
+#
+# echo "################################################################"
+#
+# echo "original_resnet_v2.tflite"
+# ~/kbench/scripts/kbench.py tflite \
+# ~/benchmark-tfl/build/rpi/ \
+# --model-and-label ~/copy-conv-approximation/dump_parser/original_resnet_v2.tflite ~/conv-approximation/dump_parser/labels.txt \
+# --inferences 8 \
+# --device-type raspbian \
+# --device-host beholder \
+# --energy \
+# --ignore-tunnel
+#
+# echo "################################################################"
+#
+# echo "approximate_resnet_v2.tflite"
+# ~/kbench/scripts/kbench.py tflite \
+# ~/benchmark-tfl/build/rpi/ \
+# --model-and-label ~/copy-conv-approximation/dump_parser/approximate_resnet_v2.tflite ~/conv-approximation/dump_parser/labels.txt \
+# --inferences 8 \
+# --device-type raspbian \
+# --device-host beholder \
+# --energy \
+# --ignore-tunnel
+#
+# echo "################################################################"
+#
+# echo "original_inception_resnet_v2.tflite"
+# ~/kbench/scripts/kbench.py tflite \
+# ~/benchmark-tfl/build/rpi/ \
+# --model-and-label ~/copy-conv-approximation/dump_parser/original_inception_resnet_v2.tflite ~/conv-approximation/dump_parser/labels.txt \
+# --inferences 8 \
+# --device-type raspbian \
+# --device-host beholder \
+# --energy \
+# --ignore-tunnel
+#
+# echo "################################################################"
+
+echo "approximate_inception_resnet_v2.tflite"
 ~/kbench/scripts/kbench.py tflite \
 ~/benchmark-tfl/build/rpi/ \
---model-and-label ~/conv-approximation/dump_parser/reconstructed_mobilenet_v2.tflite ~/conv-approximation/dump_parser/labels.txt \
+--model-and-label ~/copy-conv-approximation/dump_parser/approximate_inception_resnet_v2.tflite ~/conv-approximation/dump_parser/labels.txt \
 --inferences 8 \
 --device-type raspbian \
 --device-host beholder \
