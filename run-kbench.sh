8a986bcf2b7da3903194c6843468ea42e4f2bc57
SusinMat <57694766+SusinMat@users.noreply.github.com>
2019-08-27 21:31:19 -0300

    Separate original and approximate benchmark results into files

--- run-kbench.sh
+++ run-kbench.sh
@@ -60,13 +60,13 @@
 # --ignore-tunnel
 #
 # echo "################################################################"
-
-echo "approximate_inception_resnet_v2.tflite"
-~/kbench/scripts/kbench.py tflite \
-~/benchmark-tfl/build/rpi/ \
---model-and-label ~/copy-conv-approximation/dump_parser/approximate_inception_resnet_v2.tflite ~/conv-approximation/dump_parser/labels.txt \
---inferences 8 \
---device-type raspbian \
---device-host beholder \
---energy \
---ignore-tunnel
+#
+# echo "approximate_inception_resnet_v2.tflite"
+# ~/kbench/scripts/kbench.py tflite \
+# ~/benchmark-tfl/build/rpi/ \
+# --model-and-label ~/copy-conv-approximation/dump_parser/approximate_inception_resnet_v2.tflite ~/conv-approximation/dump_parser/labels.txt \
+# --inferences 8 \
+# --device-type raspbian \
+# --device-host beholder \
+# --energy \
+# --ignore-tunnel
